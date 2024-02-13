2dc77fc7a754630b31d70a5973db7c5c0614f030
ian-lim-intel <156939744+ian-lim-intel@users.noreply.github.com>
2024-02-13 09:49:00 +0000

    Updated App

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.spider"
-    compileSdk = 19
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.spider"
         minSdk = 19
-        targetSdk = 19
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -35,4 +35,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("androidx.core:core:1.7.0")
+
 }
--- app/src/main/java/www/terasic/com/tw/SPIDER/startdisplay.java
+++ app/src/main/java/www/terasic/com/tw/SPIDER/startdisplay.java
@@ -1,5 +1,8 @@
 package www.terasic.com.tw.SPIDER;
 
+import androidx.core.app.ActivityCompat;
+import android.content.pm.PackageManager;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,9 +14,48 @@ import com.example.spider.R;
 import java.text.SimpleDateFormat;
 
 public class startdisplay extends Activity{
+	private static String[] PERMISSIONS_STORAGE = {
+			android.Manifest.permission.READ_EXTERNAL_STORAGE,
+			android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+			android.Manifest.permission.ACCESS_FINE_LOCATION,
+			android.Manifest.permission.ACCESS_COARSE_LOCATION,
+			android.Manifest.permission.ACCESS_LOCATION_EXTRA_COMMANDS,
+			android.Manifest.permission.BLUETOOTH_SCAN,
+			android.Manifest.permission.BLUETOOTH_CONNECT,
+			android.Manifest.permission.BLUETOOTH_PRIVILEGED
+	};
+	private static String[] PERMISSIONS_LOCATION = {
+			android.Manifest.permission.ACCESS_FINE_LOCATION,
+			android.Manifest.permission.ACCESS_COARSE_LOCATION,
+			android.Manifest.permission.ACCESS_LOCATION_EXTRA_COMMANDS,
+			android.Manifest.permission.BLUETOOTH_SCAN,
+			android.Manifest.permission.BLUETOOTH_CONNECT,
+			android.Manifest.permission.BLUETOOTH_PRIVILEGED
+	};
+
+	private void checkPermissions(){
+		int permission1 = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+		int permission2 = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_SCAN);
+		if (permission1 != PackageManager.PERMISSION_GRANTED) {
+			// We don't have permission so prompt the user
+			ActivityCompat.requestPermissions(
+					this,
+					PERMISSIONS_STORAGE,
+					1
+			);
+		} else if (permission2 != PackageManager.PERMISSION_GRANTED){
+			ActivityCompat.requestPermissions(
+					this,
+					PERMISSIONS_LOCATION,
+					1
+			);
+		}
+	}
+
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.startdisplay);
+		checkPermissions();
 		 new Thread(new Runnable(){
 
 	            @Override
